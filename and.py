215f62d328165233be305a8733d8522281514753
hariprakash619 <hariprakash.619@gmail.com>
2021-09-26 13:37:33 +0530

    loggig added

--- and.py
+++ and.py
@@ -1,21 +1,26 @@
 from utils.model import Perceptron
 from utils.all_utils import prepare_data
+from utils.all_utils import save_plot
+from utils.all_utils import save_model
 import pandas as pd
 import numpy as np
 
-AND = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y":  [0,0,0,1]
-}
-df = pd.DataFrame(AND)
+def main(data, modelName, plotName, eta, epochs):
+    df = pd.DataFrame(data)
+    print(df)
+    X, y = prepare_data(df)
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
+    _ = model.total_loss()
+    save_model(model, filename=modelName)
+    save_plot(df, plotName, model)
 
-X,y = prepare_data(df)
-
-ETA = 0.3
-EPOCHS = 10
-
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X,y)
-
-_ = model.total_loss()
+if __name__ == '__main__':
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1],
+    }
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+    main(data=AND, modelName="and.model", plotName="and.png", eta=ETA, epochs=EPOCHS)
--- utils/model.py
+++ utils/model.py
@@ -1,53 +1,45 @@
-
-
+import numpy as np
+import logging
+from tqdm import tqdm
 
 class Perceptron:
-  #eta - learning rate the scalar value
-
   def __init__(self, eta, epochs):
-    self.weights = np.random.randn(3) * 1e-4 # 3 for 3 inputs
-    print(f"intial weights before training: {self.weights}")
-    self.eta = eta
-    self.epochs = epochs
+    self.weights = np.random.randn(3) * 1e-4 # SMALL WEIGHT INIT
+    logging.info(f"initial weights before training: \n{self.weights}")
+    self.eta = eta # LEARNING RATE
+    self.epochs = epochs 
 
-  def activationFunction(self, inputs, weights):   #sigmoid function
-    z= np.dot(inputs,weights) # z = W * X
-    return np.where(z > 0, 1, 0)
-  #fit method training steps
 
-  def fit(self,X,y):
+  def activationFunction(self, inputs, weights):
+    z = np.dot(inputs, weights) # z = W * X
+    return np.where(z > 0, 1, 0) # CONDITION, IF TRUE, ELSE
+
+  def fit(self, X, y):
     self.X = X
     self.y = y
-  # self can be used any wher ein the class a global variable
-    X_with_bias = np.c_[self.X,-np.ones((len(self.X),1))]
-    print(f"X with bias :\n {X_with_bias}")
-    for epoch in range(self.epochs):
-      print("--"*10)
-      print(f"for eposch : {epoch}")
-      print("--"*10)
-      #forward propagation 
-      y_hat = self.activationFunction(X_with_bias,self.weights)
-      print(f"predicted value after forward pass: {y_hat}")
-      self.error = self.y - y_hat;
-      print(f"error : \n {self.error}")
-      self.weights = self.weights + self.eta * np.dot(X_with_bias.T,self.error)
-      print(f"Updated weights after epoch: {epoch}/{self.epochs},weights : {self.weights}")
-      print("#####"*10)
 
-  # prediction
-  def predict(self,X):
-    X_with_bias = np.c_[X, -np.ones((len(X),1))]
+    X_with_bias = np.c_[self.X, -np.ones((len(self.X), 1))] # CONCATINATION
+    logging.info(f"X with bias: \n{X_with_bias}")
+
+    for epoch in tqdm(range(self.epochs), total=self.epochs, desc="training the model"):
+      logging.info("--"*10)
+      logging.info(f"for epoch: {epoch}")
+      logging.info("--"*10)
+
+      y_hat = self.activationFunction(X_with_bias, self.weights) # foward propagation
+      logging.info(f"predicted value after forward pass: \n{y_hat}")
+      self.error = self.y - y_hat
+      logging.info(f"error: \n{self.error}")
+      self.weights = self.weights + self.eta * np.dot(X_with_bias.T, self.error) # backward propagation
+      logging.info(f"updated weights after epoch:\n{epoch}/{self.epochs} : \n{self.weights}")
+      logging.info("#####"*10)
+
+
+  def predict(self, X):
+    X_with_bias = np.c_[X, -np.ones((len(X), 1))]
     return self.activationFunction(X_with_bias, self.weights)
-  
-  def prepare_data(df):
-    X = df.drop("y",axis=1)
-    y = df["y"]
-    return X,y
+
   def total_loss(self):
     total_loss = np.sum(self.error)
-    print(f"total loss: {total_loss}")
+    logging.info(f"total loss: {total_loss}")
     return total_loss
-
-
-
-
